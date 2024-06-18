32766aac392fb02d992edd8d38b09e2c4460ffd5
pedelman <patrick.the.edelman@gmail.com>
2024-06-18 10:12:01 -0500

    Adds Scala LSP support

--- lua/pedelman/lazy/lsp.lua
+++ lua/pedelman/lazy/lsp.lua
@@ -56,6 +56,43 @@ return {
         end
     },
     {
+        "scalameta/nvim-metals",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        ft = { "scala", "sbt", "java" },
+        opts = function()
+            local metals_config = require("metals").bare_config()
+            metals_config.on_attach = function(client, bufnr)
+                local opts = {buffer = bufnr, remap = false}
+
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+            end
+
+            metals_config.settings = {
+                showImplicitArguments = true,
+                showImplicitConversionsAndClasses = true,
+                showInferredType = true,
+                superMethodLensesEnabled = true,
+            }
+            metals_config.init_options.statusBarProvider = "on"
+            metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            return metals_config
+        end,
+        config = function(self, metals_config)
+            local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = self.ft,
+                callback = function()
+                    require("metals").initialize_or_attach(metals_config)
+                end,
+                group = nvim_metals_group,
+            })
+        end
+    },
+    {
         'neovim/nvim-lspconfig',
         cmd = {'LspInfo', 'LspInstall', 'LspStart'},
         event = {'BufReadPre', 'BufNewFile'},
