9856a8909498e116db6a608b491e6882934a9262
Patrick Edelman <patrick.the.edelman@gmail.com>
2024-06-19 12:20:11 -0500

    gx jira helper to use JIRA_HOST

--- lua/pedelman/lazy/gx.lua
+++ lua/pedelman/lazy/gx.lua
@@ -8,21 +8,21 @@ return {
 		end,
 		dependencies = { "nvim-lua/plenary.nvim" },
 
-		-- you can specify also another config if you want
 		config = function()
 			require("gx").setup({
 				handlers = {
-					plugin = true, -- open plugin links in lua (e.g. packer, lazy, ..)
-					github = true, -- open github issues
-					package_json = true, -- open dependencies from package.json
-					search = true, -- search the web/selection on the web if nothing else is found
-					go = true, -- open pkg.go.dev from an import statement (uses treesitter)
-					jira = { -- custom handler to open Jira tickets (these have higher precedence than builtin handlers)
-						name = "jira", -- set name of handler
+					plugin = true,
+					github = true,
+					package_json = true,
+					search = true,
+					go = true,
+					jira = {
+						name = "jira",
 						handle = function(mode, line, _)
 							local ticket = require("gx.helper").find(line, mode, "(%u+-%d+)")
 							if ticket and #ticket < 20 then
-								return "http://jira.bigcommerce.com/browse/" .. ticket
+								local jira_host = os.getenv("JIRA_HOST")
+								return jira_host .. "/browse/" .. ticket
 							end
 						end,
 					},
--- lua/pedelman/lazy/lsp.lua
+++ lua/pedelman/lazy/lsp.lua
@@ -160,8 +160,34 @@ return {
 					end,
 
 					lua_ls = function()
-						local lua_opts = lsp_zero.nvim_lua_ls()
-						lspconfig.lua_ls.setup(lua_opts)
+						lspconfig.lua_ls.setup({
+							on_init = function(client)
+								local path = client.workspace_folders[1].name
+								if
+									vim.loop.fs_stat(path .. "/.luarc.json")
+									or vim.loop.fs_stat(path .. "/.luarc.jsonc")
+								then
+									return
+								end
+
+								client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+									runtime = {
+										version = "LuaJIT",
+									},
+									-- Make the server aware of Neovim runtime files
+									workspace = {
+										checkThirdParty = false,
+										library = {
+											vim.env.VIMRUNTIME,
+											"${3rd}/luv/library",
+										},
+									},
+								})
+							end,
+							settings = {
+								Lua = {},
+							},
+						})
 					end,
 
 					gopls = function()
